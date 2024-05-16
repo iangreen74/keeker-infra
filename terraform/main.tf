387d84b4d966a6b0e0ffdd0bb35bdd5e440741d5
Ian Green <mikahiangreen@gmail.com>
2024-05-16 07:38:32 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ module "eks" {
   version         = "~> 20.0"
   cluster_name    = "keeker-clstr"
   cluster_version = "1.29"
-  subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghij012"]
-  vpc_id          = "vpc-12345678"
+  subnets         = ["subnet-042771f1827029d88", "subnet-0d3090b9f7bdb0530", "subnet-04d35295708eb8d8d"]
+  vpc_id          = "vvpc-06b68af264b469c07"
 
   node_groups = {
     example = {
